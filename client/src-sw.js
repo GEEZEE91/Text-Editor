5e80343adf68c7f409f56440cdb6cdc29849a964
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-01-04 01:46:08 +1000

    asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,13 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute( matchCallback,
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination), 
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }));
