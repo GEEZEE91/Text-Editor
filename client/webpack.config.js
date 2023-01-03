9d9dd94518444d5e772ecbb3fde044caad53bb4e
GINA ZIVKOVIC <GINA.ZIVKOVIC91@GMAIL.COM>
2023-01-04 00:41:28 +1000

    service worker and manifest file.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,6 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -17,9 +16,42 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    
+    
     plugins: [
+      // Adding Webpack plugin
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      new MiniCssExtractPlugin(),
       
+      // add service worker
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }), 
+
+       // add manifest.json
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#272822',
+        theme_color: '#272822',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
+
 
     module: {
       // Add CSS loaders and babel to webpack
